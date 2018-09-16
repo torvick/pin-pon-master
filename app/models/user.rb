b9a78de6fc815c56a326301fc5c1212aca2bcbae
Victor <vurqudes@metricas.mx>
2018-09-16 16:37:07 -0500

    Update Models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  #has_many :games, dependent: :delete_all
-  #has_many :ratings, dependent: :delete_all
+  has_many :games, dependent: :delete_all
+  has_many :ratings, dependent: :delete_all
 end
